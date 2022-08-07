c8496a4904dee50fd0aa18fcc3202a0f38a5c59f
Chris Hollis-Locke <chl@sporelab.io>
2022-08-08 09:29:02 +1200

    Set UDP transport defaults from Kconfig settings.
    Was originally hard-wired, with the Kconfig settings having no effect.

--- modules/libmicroros/microros_transports/udp/microros_transports.h
+++ modules/libmicroros/microros_transports/udp/microros_transports.h
@@ -33,7 +33,7 @@ typedef struct {
 } zephyr_transport_params_t;
 
 #define MICRO_ROS_FRAMING_REQUIRED false
-static zephyr_transport_params_t default_params = {.ip = "192.168.1.100", .port = "8888"};
+static zephyr_transport_params_t default_params = {.ip = CONFIG_MICROROS_AGENT_IP, .port = CONFIG_MICROROS_AGENT_PORT};
 
 bool zephyr_transport_open(struct uxrCustomTransport * transport);
 bool zephyr_transport_close(struct uxrCustomTransport * transport);
